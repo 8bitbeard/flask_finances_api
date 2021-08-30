b5c6729cd9ce89ad85702de7ded73c4d8ea6f1b6
Wilton Souza <willsouzafilho@gmail.com>
2021-08-30 20:12:04 -0300

    Trying to mock os.environ

--- tests/unit/test_create_db.py
+++ tests/unit/test_create_db.py
@@ -1,5 +1,6 @@
 def test_create_db(mocker):
     mock = mocker.patch('src.database.create_db.db').return_value = mocker.Mock()
-    mocker.patch('src.database.create_db.os.environ').return_value = mocker.Mock
+    mocker.patch('src.database.create_db.create_app').return_value = mocker.Mock()
+    mocker.patch('src.database.create_db.os.environ').return_value = mocker.Mock()
     mock.assert_called_once
 
