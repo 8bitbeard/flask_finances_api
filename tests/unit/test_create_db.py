e634611c2bbdede2314e395916ab96259ad59b32
Wilton Souza <willsouzafilho@gmail.com>
2021-08-30 20:08:30 -0300

    Mocking create_app on create_db script test

--- tests/unit/test_create_db.py
+++ tests/unit/test_create_db.py
@@ -1,4 +1,5 @@
 def test_create_db(mocker):
     mock = mocker.patch('src.database.create_db.db').return_value = mocker.Mock()
+    mocker.patch('src.database.create_db.os.environ').return_value = mocker.Mock
     mock.assert_called_once
 
