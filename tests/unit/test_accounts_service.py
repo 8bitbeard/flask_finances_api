acb258610887ca281c0f9e3efb4a22fd9c97b282
Wilton Souza <willsouzafilho@gmail.com>
2021-08-30 11:24:05 -0300

    Adding docstrings and class separation to test_accounts_service.py

--- tests/unit/test_accounts_service.py
+++ tests/unit/test_accounts_service.py
@@ -1,3 +1,10 @@
+# type: ignore
+
+"""
+Test file for accounts_service.py file
+"""
+
+
 import pytest
 
 from src.services.accounts_service import AccountsService
@@ -6,53 +13,94 @@ from src.exceptions.accounts_exception import AccountNotFound, AccountInvalidNam
 from src.exceptions.users_exception import UserNotFound
 from src.exceptions.balance_exception import BalanceInvalid
 
-def test_create_error_name_less_than_3_chars():
-    with pytest.raises(AccountInvalidName):
-        AccountsService.create('id', {'name': 'in', 'balance': 12.25})
 
-def test_create_error_user_not_found(mock_get_sqlalchemy):
-    mock_get_sqlalchemy.filter_by.return_value.first.return_value = None
-    with pytest.raises(UserNotFound):
-        AccountsService.create('id', {'name': 'Account', 'balance': 12.25})
+class TestCreate:
+    """
+    Class for testing the create service method
+    """
 
-def test_create_error_account_balance_invalid(mock_get_sqlalchemy):
-    mock_get_sqlalchemy.filter_by.return_value.first.return_value = True
-    with pytest.raises(BalanceInvalid):
-        AccountsService.create('id', {'name': 'Account', 'balance': 'invalid'})
+    def test_create_error_name_less_than_3_chars(self):
+        """
+        Test if create method raises a AccountInvalidName when creating an account with less than 3 chars
+        """
+        with pytest.raises(AccountInvalidName):
+            AccountsService.create('id', {'name': 'in', 'balance': 12.25})
 
-def test_create_accounts_service_method(mock_get_sqlalchemy, mock_account_object, mocker):
-    mock_get_sqlalchemy.filter_by.return_value.first.return_value = mock_account_object
-    mocker.patch("src.services.users_service.db.session").return_value = mocker.Mock()
-    account = AccountsService.create('user_id', {'name': 'Account', 'balance': 10.25})
-    assert account.id is not None
-    assert account.user_id == 'user_id'
-    assert account.name == 'Account'
-    assert account.balance == 10.25
-    assert account.income == 0
-    assert account.expense == 0
+    def test_create_error_user_not_found(self, mock_get_sqlalchemy):
+        """
+        Test if create method raises a UserNotFound when creating an account for an inexistent user
+        """
+        mock_get_sqlalchemy.filter_by.return_value.first.return_value = None
+        with pytest.raises(UserNotFound):
+            AccountsService.create('id', {'name': 'Account', 'balance': 12.25})
 
-def test_index_accounts_service_method(mock_get_sqlalchemy, mock_account_object):
-    mock_get_sqlalchemy.filter_by.return_value = [mock_account_object]
-    accounts = AccountsService.index('user_id')
-    assert isinstance(accounts, list)
-    assert accounts[0].id is not None
-    assert accounts[0].user_id is not None
-    assert accounts[0].name == 'Mock Account'
-    assert accounts[0].balance == 10.25
-    assert accounts[0].income == 0
-    assert accounts[0].expense == 0
+    def test_create_error_account_balance_invalid(self, mock_get_sqlalchemy):
+        """
+        Test if create method raises a BalanceInvalid when creating an account with invalid value
+        """
+        mock_get_sqlalchemy.filter_by.return_value.first.return_value = True
+        with pytest.raises(BalanceInvalid):
+            AccountsService.create('id', {'name': 'Account', 'balance': 'invalid'})
 
-def test_retrieve_error_account_not_found(mock_get_sqlalchemy):
-    mock_get_sqlalchemy.filter_by.return_value.first.return_value = None
-    with pytest.raises(AccountNotFound):
-        AccountsService.retrieve('user_id', 'account_id')
+    def test_create_accounts_service_method(self, mock_get_sqlalchemy, mock_account_object, mocker):
+        """
+        Test if create method creates an account successfully
+        """
+        mock_get_sqlalchemy.filter_by.return_value.first.return_value = mock_account_object
+        mocker.patch("src.services.users_service.db.session").return_value = mocker.Mock()
+        account = AccountsService.create(mock_account_object.user_id,
+                                         {'name': mock_account_object.name, 'balance': mock_account_object.balance})
+        assert account.id is not None
+        assert account.user_id == mock_account_object.user_id
+        assert account.name == mock_account_object.name
+        assert account.balance == mock_account_object.balance
+        assert account.income == mock_account_object.income
+        assert account.expense == mock_account_object.expense
 
-def test_retrieve_accounts_service_method(mock_get_sqlalchemy, mock_account_object):
-    mock_get_sqlalchemy.filter_by.return_value.first.return_value = mock_account_object
-    account = AccountsService.retrieve('user_id', 'account_id')
-    assert account.id is not None
-    assert account.user_id is not None
-    assert account.name == 'Mock Account'
-    assert account.balance == 10.25
-    assert account.income == 0
-    assert account.expense == 0
+
+class TestIndex:
+    """
+    Class for testing the index service method
+    """
+
+    def test_index_accounts_service_method(self, mock_get_sqlalchemy, mock_account_object):
+        """
+        Test if index method returns a list of accounts
+        """
+        mock_get_sqlalchemy.filter_by.return_value = [mock_account_object]
+        accounts = AccountsService.index('user_id')
+        assert isinstance(accounts, list)
+        assert accounts[0].id == mock_account_object.id
+        assert accounts[0].user_id  == mock_account_object.user_id
+        assert accounts[0].name == mock_account_object.name
+        assert accounts[0].balance == mock_account_object.balance
+        assert accounts[0].income == mock_account_object.income
+        assert accounts[0].expense == mock_account_object.expense
+
+
+class TestRetrieve:
+    """
+    Class for testing the retrieve service method
+    """
+
+    def test_retrieve_error_account_not_found(self, mock_get_sqlalchemy):
+        """
+        Test if retrieve method returns an AccountNotFound for an inexistent user
+        """
+        mock_get_sqlalchemy.filter_by.return_value.first.return_value = None
+        with pytest.raises(AccountNotFound):
+            AccountsService.retrieve('user_id', 'account_id')
+
+    def test_retrieve_accounts_service_method(self, mock_get_sqlalchemy, mock_account_object):
+        """
+        Test if retrieve method returns a account object
+        """
+        mock_get_sqlalchemy.filter_by.return_value.first.return_value = mock_account_object
+        account = AccountsService.retrieve('user_id', 'account_id')
+        assert account.id == mock_account_object.id
+        assert account.user_id  == mock_account_object.user_id
+        assert account.name == mock_account_object.name
+        assert account.balance == mock_account_object.balance
+        assert account.income == mock_account_object.income
+        assert account.expense == mock_account_object.expense
+
