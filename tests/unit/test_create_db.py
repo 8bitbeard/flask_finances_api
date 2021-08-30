4719e1e3d42d9d9fbedc71f784e1ef2c9849de46
Wilton Souza <willsouzafilho@gmail.com>
2021-08-30 20:14:10 -0300

    Adding FLASK_ENV to github workflow env

--- tests/unit/test_create_db.py
+++ tests/unit/test_create_db.py
@@ -1,6 +1,4 @@
 def test_create_db(mocker):
     mock = mocker.patch('src.database.create_db.db').return_value = mocker.Mock()
-    mocker.patch('src.database.create_db.create_app').return_value = mocker.Mock()
-    mocker.patch('src.database.create_db.os.environ').return_value = mocker.Mock()
     mock.assert_called_once
 
